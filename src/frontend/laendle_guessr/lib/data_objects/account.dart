f937867590404f749305dcfac45bf31eabaf4719
dinomarlon187 <marlon.pichler08@gmail.com>
2025-05-09 10:42:43 +0200

    Empty classes

--- src/frontend/laendle_guessr/lib/data_objects/account.dart
+++
@@ -1,6 +0,0 @@
-abstract class Account {
-  String username;
-  String email;
-  int coins;
-  Account({required this.username, required this.email, required this.coins});
-}
--- src/frontend/laendle_guessr/lib/data_objects/item.dart
+++
@@ -1,9 +0,0 @@
-class Item{
-  int iid;
-  String name;
-  String description;
-  int price;
-  String imageURL;
-
-  Item({required this.iid, required this.name, required this.description, required this.price, required this.imageURL});
-}
